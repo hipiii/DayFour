660c816828cb731d5b8cb85432cd3126e7cd6af4
hipii <durgamagar1098@gmail.com>
2024-12-19 16:01:47 +0545

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -32,15 +32,19 @@ function App() {
 
   const [count, setCount] = useState(0)
 
+  function Submit() {
+    alert(username + ' ' + email + ' ' + password + ' ' +confirmpassword);
+}
+
   const Schemas = yup.object().shape({
     Username: yup.string().required("This is required"),
     Email: yup.string().email("Please provide a valid email").required("Email is required"),
-    Password: yup.string().max(8,"Password must be at least 8 characters").required("Password is required"),
-    Confirmpassword: yup.string().oneOf([yup.ref("password")], "password not match").required("This is required")
+    Password: yup.string().max(8, "Password must be at least 8 characters").required("Password is required"),
+    Confirmpassword: yup.string().oneOf([yup.ref("password")], "password not match").required("Confirmed password is required")
   })
 
   return (
-    <>
+    <div className=' flex p-8 justify-center items-center '>
 
       <Formik initialValues={{
         Username: "",
@@ -48,31 +52,35 @@ function App() {
         Password: "",
         Confirmpassword: ""
       }}
-      validationSchema={Schemas}
-      onSubmit={(values)=>{
-        console.log(values)
-      }}>
-        <Form className=''>
-          <div>
-            {
+        validationSchema={Schemas}
+        onSubmit={(values) => {
+          console.log(values)
+        }}>
+        <Form>
+          <div className='bg-gradient-to-t from-pink-300 to-gray-300 flex flex-col px-8 py-2 rounded-3xl '>
+            <div className='flex flex-row  font-bold justify-center text-red-600 text-2xl'>
+              <h1>Login</h1>
+            </div>
+            <div className='flex flex-col gap-1 px-4 py-2 '>{
               form.map((val, i) => {
                 return (
-                  <div key={i}>
+                  <div key={i} className='flex flex-col mx-3 my-3'>
                     {val.name}
-                    <Field name={val.name} type={val.type} placeholder={val.placeholder} />
+                    <Field name={val.name} type={val.type} className='px-1' placeholder={val.placeholder} />
                     <ErrorMessage name={val.name} component={'div'} className='text-red-500' />
                   </div>
                 )
               })
             }
-            <div>
-              <button type='submit'>Submit</button>
+              <div className='flex justify-center gap-3 mx-4 my-2'>
+                <button type='submit' className='  flex flex-wrap justify-center px-2 py-1 bg-red-500  rounded-2xl items-center'>Submit</button>
+              </div>
             </div>
           </div>
-        </Form>
-      </Formik>
+      </Form>
+    </Formik>
 
-    </>
+    </div >
   )
 }
 
